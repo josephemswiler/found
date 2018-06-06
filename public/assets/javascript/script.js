8cd2808745cb1fc97e9223ab4de57dae823c44fb
josephemswiler <josephemswiler@gmail.com>
2018-06-06 08:14:56 -0500

    Talk screen init

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -2,4 +2,23 @@ console.log('hello world')
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+    $('html, body').animate({
+        scrollTop: 0
+    })
+})
+
+//Dynamically set max-height of list based on window size
+//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//
+function setHeight() {
+    let innerHeight = $(window).height() - 86
+    $('.body-wrapper').css('height', innerHeight)
+}
+$(document).ready(setHeight())
+
+$(window).resize(function () {
+    setHeight()
+})
+
+$(document).on('click', '.talk-btn', function() {
+    // $('.first-row').css('height', //here)
+})
